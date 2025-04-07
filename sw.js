478229a2699198fa674b7bce7132f2f6b65cbe93
Eduardo da Silva <78367379+Silvaedoo@users.noreply.github.com>
2025-04-07 13:25:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,51 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('nosso-tempo-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+const CACHE_NAME = "meu-tempo-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./fundo.mp4",
+  "./c1.jpeg",
+  "./c2.jpeg",
+  "./c3.jpeg",
+  "./c4.jpeg",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Instalando o Service Worker e armazenando os arquivos no cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+// Ativando o Service Worker e limpando caches antigos, se houver
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e responde com cache ou rede
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
